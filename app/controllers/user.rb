b9bde15124f25a9f6add167c51aa81f01cc15c47
jonnorstrom <jon.norstrom@gmail.com>
2016-04-28 13:30:25 -0500

    Users and register and login

---
+++ app/controllers/user.rb
@@ -0,0 +1,15 @@
+post '/users' do
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      @error = "Whoops! Remember to fill out both the username and password fields, usernames must be unique!"
+      erb :login
+    end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :"users/show"
+end
