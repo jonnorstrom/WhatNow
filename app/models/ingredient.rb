7e07d64457fdc8c3193a84109c26e97fccc6d49f
jonnorstrom <jon.norstrom@gmail.com>
2016-04-28 11:04:55 -0500

    skeleton set up

--- app/models/ingredient.rb
+++
@@ -1,5 +0,0 @@
-class Ingredient < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :recipe ## good to go
-  belongs_to :item ## good to go
-end
--- db/migrate/20160424154801_update_recipes_foods_table.rb
+++
@@ -1,5 +0,0 @@
-class UpdateRecipesFoodsTable < ActiveRecord::Migration
-  def change
-    rename_column(:recipe_foods, :food_item_id, :food_id)
-  end
-end
