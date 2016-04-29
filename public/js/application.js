85c3060ad31db3c9dfba5d25873ae468aeaba631
jonnorstrom <jon.norstrom@gmail.com>
2016-04-28 23:50:45 -0500

    THIS APP WORKS

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,15 @@
-var moods = { happy: ['Will Ferrell','Sandra Bullock','Adam Sandler'],
-  sad: ['Classic Movies', 'Romance','Dramas'],
-  shitty: ['Independent Movies','Thrillers','Faith & Spirtuality']
+var moods = { happy: ['Comedies', 'Children & Family Movies'],
+  sad: ['Classic Movies', 'Oscar-winning Movies', 'Dramas'],
+  shitty: ['Independent Movies','Thrillers'],
+  laughing: ['Comedies', 'Cult Movies'],
+  spooky: ['Horror Movies'],
+  pissed: ['Action & Adventure', 'Thrillers'],
+  wine: ['Oscar-winning Movies', 'Classic Movies', 'Gay & Lesbian Movies', 'Documentaries'],
+  inlove: ['Romantic Movies', 'Gay & Lesbian Movies'],
+  scary: ['Thrillers', 'Action & Adventure', 'Sci-Fi & Fantasy'],
+  beers: ['Comedies', 'TV Shows', 'Cult Movies', 'Action & Adventure'],
+  british: ['Foreign Movies', 'Classic Movies'],
+  thinker: ['Documentaries', 'Independent Movies', 'Foreign Movies', 'Sci-Fi & Fantasy', 'Romantic Movies', 'Comedies', 'TV Shows']
 };
 
 Array.prototype.sample = function(){
@@ -33,10 +42,11 @@ $(document).ready(function(){
     });
   });
 
+// event handler for emoji mood!
   $('.mood-table').on('click', 'img', function(){
     var mood = $(this).attr('id');
-    var genre = moods[mood].sample();
-    var data = {actor: genre};
+    var data = {genre: moods[mood].sample()};
+    console.log(data)
     var request = $.ajax({
       url: '/getflix',
       data: data
@@ -46,5 +56,5 @@ $(document).ready(function(){
       $('.emojis').addClass('hidden');
       $('.main-container').append(responseHtml);
     });
-  });
+  }); // end of mood event handler
 });
